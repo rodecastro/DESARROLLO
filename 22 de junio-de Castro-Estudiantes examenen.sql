dca3d5fa62b1eba0c5945fbd2d2a1052fe3e4c81
rodecastro <80895654+rodecastro@users.noreply.github.com>
2021-06-29 08:55:42 -0300

    Add files via upload

---
+++ 22 de junio-de Castro-Estudiantes examenen.sql
@@ -0,0 +1,6 @@
+create database estudiantes;
+create table examen (idest int  not null primary key,
+nombre varchar(12) not null,
+apellido varchar(12) not null,
+nota int not null,
+curso varchar(40) not null);
